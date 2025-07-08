fdd4d77adf8d9553c4581e14a3e8d39e490c01f0
Kenneth C. Walker Jr. <130890117+kwalker7631@users.noreply.github.com>
2025-07-08 01:57:43 -0400

    Add files via upload

---
+++ custom_patterns.py
@@ -0,0 +1,15 @@
+# custom_patterns.py
+# This file stores user-defined regex patterns.
+
+MODEL_PATTERNS = [
+    r'\bFS\-\d+DN\b',
+    r'\bKM\-\d+\b',
+    r'\bKM\-C\d+\b',
+    r'\bKM\-C\d+E\b',
+    r'\bEP\ C\d+DN\b',
+    r'\bFS\-C\d+DN\b',
+    r'\bKM\-\d+w\b',
+]
+
+QA_NUMBER_PATTERNS = [
+]
